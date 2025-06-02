2506df7a7a64aa5990d0714c40bd75d69a0177d4
Akerlox <125981924+Akerlox54@users.noreply.github.com>
2025-06-02 20:55:41 +0700

    Add play a round feature. Feature: throw an error when choices are undefined. Optimize code a bit.

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-let computerChoice;
-let humanChoice;
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice () {
+    let result;
     let randomNum = Math.floor(Math.random() * 3) + 1;
 
-    if (randomNum === 1) {computerChoice = 'rock';}
-    else if (randomNum === 2) {computerChoice ='paper';}
-    else if (randomNum === 3) {computerChoice = 'scissor';}
+    if (randomNum === 1) {result = 'rock'; }
+    else if (randomNum === 2) {result ='paper'; }
+    else if (randomNum === 3) {result = 'scissor'; }
     else alert('An error occurred!');
-    return computerChoice;
+    return result;
 }
 
 function getHumanChoice() {
-    humanChoice = prompt('What do you choose?', 'Rock? Paper? Scissor?').toLowerCase();
-    if (humanChoice === 'rock'
-        || humanChoice === 'paper'
-        || humanChoice === 'scissor') {
-            return humanChoice;
-        }
+    let result = prompt('What do you choose?', 'Rock? Paper? Scissor?').toLowerCase();
+    if (result === 'rock'
+        || result === 'paper'
+        || result === 'scissor') {
+            return result; }
     else alert('Invalid choice.');
+}
+
+function playRound(humanChoice, computerChoice) {
+    console.log(humanChoice, computerChoice);
+    if (humanChoice === computerChoice
+        && humanChoice !== null && humanChoice !== undefined
+        && computerChoice !== null && humanChoice !== undefined
+    ) {alert('Draw!'); }
+    else if (humanChoice === 'rock' && computerChoice === 'scissor'
+        || humanChoice === 'paper' && computerChoice === 'rock'
+        || humanChoice === 'scissor' && computerChoice === 'paper'
+    ) {
+        alert('Human wins!');
+    humanScore =+ 1; }
+    else if (computerChoice === 'rock' && humanChoice === 'scissor'
+        || computerChoice === 'paper' && humanChoice === 'rock'
+        || computerChoice === 'scissor' && humanChoice === 'paper'
+    ) {
+        alert('Computer wins!');
+    computerScore =+ 1; }
+    else alert('An error occurred!')
 }
