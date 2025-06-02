2377cc7732a70b8c3fc3fe2e57b8b7e628fefbc1
Akerlox <125981924+Akerlox54@users.noreply.github.com>
2025-06-02 20:32:47 +0700

    Feat: get human's choice. Optimize get computer's choice feature.

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 let computerChoice;
-let human
+let humanChoice;
+
 function getComputerChoice () {
     let randomNum = Math.floor(Math.random() * 3) + 1;
 
-    if (randomNum === 1) {computerChoice = 'rock';
-        return computerChoice;
-    }
-    else if (randomNum === 2) {computerChoice ='paper';
-        return computerChoice;
-    }
-    else if (randomNum === 3) {computerChoice = 'scissor'
-        return computerChoice;
-    }
+    if (randomNum === 1) {computerChoice = 'rock';}
+    else if (randomNum === 2) {computerChoice ='paper';}
+    else if (randomNum === 3) {computerChoice = 'scissor';}
     else alert('An error occurred!');
+    return computerChoice;
+}
+
+function getHumanChoice() {
+    humanChoice = prompt('What do you choose?', 'Rock? Paper? Scissor?').toLowerCase();
+    if (humanChoice === 'rock'
+        || humanChoice === 'paper'
+        || humanChoice === 'scissor') {
+            return humanChoice;
+        }
+    else alert('Invalid choice.');
 }
